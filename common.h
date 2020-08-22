732e1841903ce82ff59db1a5d0baf707bfec01e6
BotBead.com <why.ms@live.com>
2020-08-22 21:17:47 +0800

    clean project

--- common.h
+++ common.h
@@ -6,21 +6,21 @@
 
 #define SAMPLE(ary, n) \
 do{ \
-    int i=0; \
-	srand(time(0)); \
-	(*(ary)) = (unsigned *)malloc(sizeof(unsigned) * (n)); \
-	if(*(ary))for(; i < n; ++i)*(*(ary) + i) = rand(); \
+int i=0; \
+srand(time(0)); \
+(*(ary)) = (unsigned *)malloc(sizeof(unsigned) * (n)); \
+if(*(ary))for(; i < n; ++i)*(*(ary) + i) = rand(); \
 }while(0)
 
 #define _SAMPLE(ary) \
 do{ \
-    if((ary))free((ary)); \
+if((ary))free((ary)); \
 }while(0)
 
 #define TRAVERSING(ary, n) \
 do{ \
-	int i=0; \
-    for(; i < n;++i)printf("%d : %u\n", i, ary[i]); \
+int i=0; \
+for(; i < n;++i)printf("%d : %u\n", i, ary[i]); \
 }while(0)
 
 #endif
